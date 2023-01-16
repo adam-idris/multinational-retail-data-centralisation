138356f10eb262e7574c18d08885d86acfafb5ba
Adam Idris <adam.idris@live.co.uk>
2023-01-16 08:49:34 +0000

    First commit

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,16 @@
+import database_utils
+import pandas as pd
+
+db_connector = database_utils.DatabaseConnector()
+
 class DataExtractor:
     
+    def read_rds_table(self, table_name):
+        self.table = table_name
+        return pd.read_sql_table(self.table, db_connector.init_db_engine())
+
+extract_table = DataExtractor()
+user_df = extract_table.read_rds_table('legacy_users')
+
+print(user_df['country'].value_counts())
+
--- database_utils.py
+++ database_utils.py
@@ -1,1 +1,31 @@
+import yaml
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+
 class DatabaseConnector:
+    
+    def read_db_creds(self):
+        with open('db_creds.yaml') as f:
+            self.db_creds = yaml.safe_load(f)
+            return self.db_creds
+    
+    def init_db_engine(self):
+        
+        creds = self.read_db_creds()
+        host = creds['RDS_HOST']
+        user = creds['RDS_USER']
+        passwd = creds['RDS_PASSWORD']
+        database = creds['RDS_DB_TYPE']
+        port = creds['RDS_PORT']
+        db_name = creds['RDS_DATABASE']
+
+        engine = create_engine(f"{database}://{user}:{passwd}@{host}:{port}/{db_name}")
+        return engine.connect()
+
+    def list_db_tables(self):
+        inspector = inspect(self.init_db_engine())
+        return inspector.get_table_names()
+
+
+readyaml = DatabaseConnector()
+print(readyaml.list_db_tables())
